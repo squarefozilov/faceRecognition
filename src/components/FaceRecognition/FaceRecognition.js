80d18e2af834d67ba058e63217fa814fdc5f1cd2
Abbosjon <squarefozilov@gmail.com>
2020-01-14 18:51:48 -0500

    * tested version

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -6,7 +6,7 @@ const FaceRecognition = ({imageUrl, box, showImage}) => {
         <div className="center ma">
             <div className='image-container absolute mt2'>
                 { showImage === true && (
-                    <img id='inputimage' alt={"I'm waiting for Url please Insert it!"} src={imageUrl} width="400px" height="auto" />
+                    <img id='inputimage' alt={"I'm waiting for Url please Insert it!"} src={imageUrl} width="700px" height="auto" />
                 )
                 }
                 { showImage === false && (
